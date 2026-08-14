156fde340a37227eb18f330902a314fbc1e59178
박현호 <hyunho@ati2000.co.kr>
2026-08-14 23:09:47 +0900

    feat: Ch01 WPF 예제 프로젝트 추가
    WPF 학습용 Ch01 프로젝트를 새로 추가한다.
    기본 App, MainWindow, AssemblyInfo를 구성하고
    솔루션에 프로젝트를 등록해 바로 실행할 수 있게 한다.

---
+++ Ch01/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ch01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            StackPanel stackpanel = new StackPanel();
+
+            Button button = new Button();
+            button.Content = "My Button";
+            TextBox textbox = new TextBox();
+            textbox.Text = "My Text";
+
+            stackpanel.Children.Add(button);
+            stackpanel.Children.Add(textbox);
+
+            this.Content = stackpanel;
+        }
+    }
+}
