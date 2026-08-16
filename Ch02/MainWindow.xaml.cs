a9c2300815e83029bde941adb9484f9dae0bb2d7
박현호 <hyunho@ati2000.co.kr>
2026-08-16 20:45:33 +0900

    feat: Ch02 WPF 예제 프로젝트 추가
    WPF 학습용 Ch02 프로젝트를 솔루션에 추가했다.
    - App/MainWindow와 바인딩용 BindData 클래스를 포함했다.
    - 솔루션에 Ch02를 등록해 함께 빌드되게 했다.

---
+++ Ch02/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ch02
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = new BindData("12345", 200);
+            btn.DataContext = new BindData("newbind", 500);
+        }
+    }
+
+    public class BindData
+    {
+        public string DataStr { get; set; }
+        public int DataInt { get; set; }
+
+        public BindData():this("none", 0)
+        {
+
+        }
+
+        public BindData(string s, int n)
+        {
+            DataStr = s;
+            DataInt = n;
+        }
+
+        public override string ToString()
+        {
+            return $"[{DataStr}] : [{DataInt}]";
+        }
+    }
+}
