88226d8a40e3f005a13aad59addb5ebc2a05921d
박현호 <hyunho@ati2000.co.kr>
2026-08-17 20:41:48 +0900

    feat: Ch03 데이터 바인딩 예제 추가
    WPF 학습용 Ch03 프로젝트를 새로 추가하고 솔루션에 연결한다.
    MainWindow에 여러 DataContext 범위와 바인딩 방식 예제를 구성해 데이터 바인딩 동작을 비교할 수 있게 한다.

---
+++ Ch03/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ch03
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            BindData bd = new BindData("Hello!2", 200);
+            this.DataContext = bd;
+        }
+    }
+
+    public class BindData
+    {
+        public string DataStr { get; set; }
+        public int DataInt { get; set; }
+
+        public BindData() : this("", 0)
+        {
+
+        }
+
+        public BindData(string s, int n)
+        {
+            DataStr = s;
+            DataInt = n;
+        }
+    }
+}
