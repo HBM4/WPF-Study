711f05bb75ccdeaf0df7544a2400fa7e42ea5fb6
박현호 <hyunho@ati2000.co.kr>
2026-08-17 21:31:15 +0900

    docs: XAML 바인딩 컨텍스트 주석 추가
    MainWindow XAML과 코드비하인드에 DataContext 적용 범위를 설명하는 주석을 보강했다. 각 GroupBox, TextBox, panel의 바인딩 기준을 명확히 해 학습용 예제의 이해를 높인다.

--- Ch03/MainWindow.xaml.cs
+++ Ch03/MainWindow.xaml.cs
@@ -22,13 +22,16 @@ namespace Ch03
         {
             InitializeComponent();
 
+            // 창 전체 DataContext -> BindData2, BindData4에서 사용됨
             BindData bd = new BindData("Hello!2", 200);
             this.DataContext = bd;
 
+            // panel 안쪽만 Person으로 바인딩
             panel.DataContext = per;
         }
     }
 
+    // 바인딩 테스트용 데이터 클래스
     public class BindData
     {
         public string DataStr { get; set; }
@@ -46,6 +49,7 @@ namespace Ch03
         }
     }
 
+    // 하단 이름/전화 입력 폼에 바인딩할 데이터
     public class Person
     {
         public string Name { get; set; }
